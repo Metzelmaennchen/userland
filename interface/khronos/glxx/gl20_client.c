71543039ad9c93ada39166728931a287ef5f8e86
steffen <metzelmaennchen@googlemail.com>
2015-05-18 00:36:08 +0200

    Moved some more functions into their specific c files.

--- interface/khronos/glxx/gl20_client.c
+++ interface/khronos/glxx/gl20_client.c
@@ -4,6 +4,9 @@
 #include "interface/khronos/common/khrn_client_rpc.h"
 
 
+extern void set_blend_func (CLIENT_THREAD_STATE_T *thread, GLenum srcRGB, GLenum dstRGB, GLenum srcAlpha, GLenum dstAlpha);
+
+
 GL_API void GL_APIENTRY glActiveTexture (GLenum texture)
 {
    CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
@@ -59,3 +62,52 @@ GL_API void GL_APIENTRY glBlendFuncSeparate (GLenum srcRGB, GLenum dstRGB, GLenu
    CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
    if (IS_OPENGLES_20(thread)) set_blend_func(thread, srcRGB, dstRGB, srcAlpha, dstAlpha);
 }
+
+GL_API GLuint GL_APIENTRY glCreateProgram (void)
+{
+   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
+   if (IS_OPENGLES_20(thread)) {
+      return RPC_UINT_RES(RPC_CALL0_RES(glCreateProgram_impl_20,
+                                        thread,
+                                        GLCREATEPROGRAM_ID_20));
+   }
+
+   return 0;
+}
+
+GL_API GLuint GL_APIENTRY glCreateShader (GLenum type)
+{
+   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
+   if (IS_OPENGLES_20(thread)) {
+      return RPC_UINT_RES(RPC_CALL1_RES(glCreateShader_impl_20,
+                                        thread,
+                                        GLCREATESHADER_ID_20,
+                                        RPC_ENUM(type)));
+   }
+
+   return 0;
+}
+
+GL_API void GL_APIENTRY glDeleteProgram (GLuint program)
+{
+   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
+   if (IS_OPENGLES_20(thread)) {
+      RPC_CALL1(glDeleteProgram_impl_20,
+                thread,
+                GLDELETEPROGRAM_ID_20,
+                RPC_UINT(program));
+   }
+}
+
+GL_API void GL_APIENTRY glDeleteShader (GLuint shader)
+{
+   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
+   if (IS_OPENGLES_20(thread)) {
+      RPC_CALL1(glDeleteShader_impl_20,
+                thread,
+                GLDELETESHADER_ID_20,
+                RPC_UINT(shader));
+   }
+}
+
+
--- interface/khronos/glxx/glxx_client.c
+++ interface/khronos/glxx/glxx_client.c
@@ -406,20 +406,6 @@ GL_API void GL_APIENTRY glClearColor (GLclampf red, GLclampf green, GLclampf blu
    }
 }
 
-GL_API void GL_APIENTRY glClearColorx (GLclampx red, GLclampx green, GLclampx blue, GLclampx alpha)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_11(thread)) {
-      RPC_CALL4(glClearColorx_impl_11,
-                thread,
-                GLCLEARCOLORX_ID_11,
-                RPC_FIXED(red),
-                RPC_FIXED(green),
-                RPC_FIXED(blue),
-                RPC_FIXED(alpha));
-   }
-}
-
 GL_API void GL_APIENTRY glClearDepthf (GLclampf depth)
 {
    CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
@@ -428,17 +414,6 @@ GL_API void GL_APIENTRY glClearDepthf (GLclampf depth)
                 thread,
                 GLCLEARDEPTHF_ID,
                 RPC_FLOAT(depth));
-   }
-}
-
-GL_API void GL_APIENTRY glClearDepthx (GLclampx depth)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_11(thread)) {
-      RPC_CALL1(glClearDepthx_impl_11,
-                thread,
-                GLCLEARDEPTHX_ID_11,
-                RPC_FIXED(depth));
    }
 }
 
@@ -453,80 +428,6 @@ GL_API void GL_APIENTRY glClearStencil (GLint s)
    }
 }
 
-GL_API void GL_APIENTRY glClientActiveTexture (GLenum texture)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_11(thread)) {
-      GLXX_CLIENT_STATE_T *state = GLXX_GET_CLIENT_STATE(thread);
-
-      vcos_assert(state != NULL);
-
-      if (texture >= GL_TEXTURE0 && texture < GL_TEXTURE0 + GL11_CONFIG_MAX_TEXTURE_UNITS)
-      {
-         state->active_texture.client = texture;
-         RPC_CALL1(glClientActiveTexture_impl_11,
-                thread,
-                GLCLIENTACTIVETEXTURE_ID_11,
-                RPC_ENUM(texture));
-      }
-      else
-         set_error(state, GL_INVALID_ENUM);
-   }
-}
-
-GL_API void GL_APIENTRY glClipPlanef (GLenum plane, const GLfloat *equation)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_11(thread)) {
-      RPC_CALL2_IN_CTRL(glClipPlanef_impl_11,
-                        thread,
-                        GLCLIPPLANEF_ID_11,
-                        RPC_ENUM(plane),
-                        equation,
-                        4 * sizeof(GLfloat));
-   }
-}
-
-GL_API void GL_APIENTRY glClipPlanex (GLenum plane, const GLfixed *equation)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_11(thread)) {
-      RPC_CALL2_IN_CTRL(glClipPlanex_impl_11,
-                        thread,
-                        GLCLIPPLANEX_ID_11,
-                        RPC_ENUM(plane),
-                        equation,
-                        4 * sizeof(GLfixed));
-   }
-}
-
-GL_API void GL_APIENTRY glColor4f (GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha)
-{
-   glintColor(
-      clampf(red, 0.0f, 1.0f),
-      clampf(green, 0.0f, 1.0f),
-      clampf(blue, 0.0f, 1.0f),
-      clampf(alpha, 0.0f, 1.0f));
-}
-
-GL_API void GL_APIENTRY glColor4ub (GLubyte red, GLubyte green, GLubyte blue, GLubyte alpha)
-{
-   glintColor(
-      (float)red / 255.0f,
-      (float)green / 255.0f,
-      (float)blue / 255.0f,
-      (float)alpha / 255.0f);
-}
-
-GL_API void GL_APIENTRY glColor4x (GLfixed red, GLfixed green, GLfixed blue, GLfixed alpha)
-{
-   glintColor(
-      clampf(fixed_to_float(red), 0.0f, 1.0f),
-      clampf(fixed_to_float(green), 0.0f, 1.0f),
-      clampf(fixed_to_float(blue), 0.0f, 1.0f),
-      clampf(fixed_to_float(alpha), 0.0f, 1.0f));
-}
-
 GL_API void GL_APIENTRY glColorMask (GLboolean red, GLboolean green, GLboolean blue, GLboolean alpha)
 {
    CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
@@ -539,47 +440,6 @@ GL_API void GL_APIENTRY glColorMask (GLboolean red, GLboolean green, GLboolean b
                 RPC_BOOLEAN(blue),
                 RPC_BOOLEAN(alpha));
    }
-}
-
-static bool is_color_size(GLint size)
-{
-   return size == 4;
-}
-
-static bool is_color_type(GLenum type)
-{
-   return type == GL_UNSIGNED_BYTE ||
-          type == GL_FIXED ||
-          type == GL_FLOAT;
-}
-
-static bool is_aligned( GLenum type, size_t value)
-{
-   switch (type) {
-   case GL_BYTE:
-   case GL_UNSIGNED_BYTE:
-      return GL_TRUE;
-   case GL_SHORT:
-   case GL_UNSIGNED_SHORT:
-      return (value & 1) == 0;
-   case GL_FIXED:
-   case GL_FLOAT:
-      return (value & 3) == 0;
-   default:
-      UNREACHABLE();
-      return GL_FALSE;
-   }
-}
-
-GL_API void GL_APIENTRY glColorPointer (GLint size, GLenum type, GLsizei stride, const GLvoid *pointer)
-{
-   if (is_color_type(type)) {
-      if (is_color_size(size) && is_aligned(type, (size_t)pointer) && is_aligned(type, (size_t)stride) && stride >= 0) {
-         glintAttribPointer(GLXX_API_11, GL11_IX_COLOR, size, type, GL_TRUE, stride, pointer);
-      } else
-         glxx_set_error_api(GLXX_API_11, GL_INVALID_VALUE);
-   } else
-      glxx_set_error_api(GLXX_API_11, GL_INVALID_ENUM);
 }
 
 static uint32_t get_palette_size(GLenum internalformat)
@@ -790,31 +650,6 @@ GL_API void GL_APIENTRY glCopyTexSubImage2D (GLenum target, GLint level, GLint x
    }
 }
 
-GL_API GLuint GL_APIENTRY glCreateProgram (void)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_20(thread)) {
-      return RPC_UINT_RES(RPC_CALL0_RES(glCreateProgram_impl_20,
-                                        thread,
-                                        GLCREATEPROGRAM_ID_20));
-   }
-
-   return 0;
-}
-
-GL_API GLuint GL_APIENTRY glCreateShader (GLenum type)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_20(thread)) {
-      return RPC_UINT_RES(RPC_CALL1_RES(glCreateShader_impl_20,
-                                        thread,
-                                        GLCREATESHADER_ID_20,
-                                        RPC_ENUM(type)));
-   }
-
-   return 0;
-}
-
 GL_API void GL_APIENTRY glCullFace (GLenum mode)
 {
    CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
@@ -866,28 +701,6 @@ GL_API void GL_APIENTRY glDeleteBuffers (GLsizei n, const GLuint *buffers)
          offset += batch;
          n -= batch;
       } while (n > 0);
-   }
-}
-
-GL_API void GL_APIENTRY glDeleteProgram (GLuint program)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_20(thread)) {
-      RPC_CALL1(glDeleteProgram_impl_20,
-                thread,
-                GLDELETEPROGRAM_ID_20,
-                RPC_UINT(program));
-   }
-}
-
-GL_API void GL_APIENTRY glDeleteShader (GLuint shader)
-{
-   CLIENT_THREAD_STATE_T *thread = CLIENT_GET_THREAD_STATE();
-   if (IS_OPENGLES_20(thread)) {
-      RPC_CALL1(glDeleteShader_impl_20,
-                thread,
-                GLDELETESHADER_ID_20,
-                RPC_UINT(shader));
    }
 }
 
